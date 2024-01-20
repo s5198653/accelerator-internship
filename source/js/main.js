88042e714e98db2f1ab03b2f3b40d3ae744594be
Taranovskaya <s5198653@yandex.ru>
2024-01-20 15:45:24 +0300

    Добавляет модальное окно

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,8 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initHeroSwiper} from './modules/hero-swiper';
 import {initToggleMenu} from './modules/burger-menu';
 import {StickyHeader} from './modules/sticky-header';
+import {initModals} from './modules/modals/init-modals';
+import {initPhoneMask} from './modules/phone-mask';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
@@ -10,6 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
     initToggleMenu();
     const stickyHeader = new StickyHeader();
     stickyHeader.init();
+    initModals();
+    initPhoneMask();
 
     // const form = new Form();
     // window.form = form;
