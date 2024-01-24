58b1f675d6672f67ac4592e97c0d0a4f4eee42e9
Taranovskaya <s5198653@yandex.ru>
2024-01-24 22:12:30 +0300

    Добавляет блок faq

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,13 @@
 import {iosVhFix} from './utils/ios-vh-fix';
+import {Form} from './vendor/form-validate/form';
 import {initHeroSwiper} from './modules/hero-swiper';
 import {initToggleMenu} from './modules/burger-menu';
 import {StickyHeader} from './modules/sticky-header';
-import {initModals} from './modules/modals/init-modals';
+import {initModals} from './modals/init-modals';
 import {initPhoneMask} from './modules/phone-mask';
 import {initProgramsSwiper} from './modules/programs-swiper';
+import {initNewsSwiper} from './modules/news-swiper';
+import {initAccordions} from './accordion/init-accordion';
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
@@ -16,10 +19,12 @@ window.addEventListener('DOMContentLoaded', () => {
     initModals();
     initPhoneMask();
 
-    // const form = new Form();
-    // window.form = form;
-    // form.init();
+    const form = new Form();
+    window.form = form;
+    form.init();
     initHeroSwiper();
     initProgramsSwiper();
+    initNewsSwiper();
+    initAccordions();
   });
 });
