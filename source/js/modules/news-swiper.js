85d3008f8855c9aad796b7deb8253a821fd3a290
Taranovskaya <s5198653@yandex.ru>
2024-01-29 17:14:41 +0300

    Добавляет блок news

---
+++ source/js/modules/news-swiper.js
@@ -0,0 +1,57 @@
+import Swiper from 'swiper';
+import {Navigation, Pagination, Keyboard, Grid} from 'swiper/modules';
+
+const initNewsSwiper = () =>
+  new Swiper('[data-swiper="news-swiper"]', {
+    modules: [Navigation, Pagination, Grid, Keyboard],
+    loop: false,
+    navigation: {
+      nextEl: '[data-button="news-next"]',
+      prevEl: '[data-button="news-prev"]',
+    },
+    pagination: {
+      el: '.news__swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<span class="${className}">${index + 1}</span>`;
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    breakpoints: {
+      1440: {
+        slidesPerView: 'auto',
+        slidesPerGroup: 1,
+        spaceBetween: 32,
+        grid: {
+          rows: 1,
+          fill: 'row',
+        },
+        allowTouchMove: false,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+
+        grid: {
+          rows: 2,
+          fill: 'column',
+        },
+        allowTouchMove: true,
+      },
+      320: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+
+        grid: {
+          rows: 2,
+          fill: 'column',
+        },
+        allowTouchMove: true,
+      },
+    },
+  });
+
+export {initNewsSwiper};
